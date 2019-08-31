e92fde28ef1a928fd46a566000c2bc21cba29f67
Banee Ishaque K <baneeishaque@gmail.com>
2019-08-31 17:06:39 +0530

    In

---
+++ app/src/main/java/ndk/ccetv_jaseem/broadcast_player/VideoActivity.java
@@ -0,0 +1,31 @@
+package ndk.ccetv_jaseem.broadcast_player;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.VideoView;
+
+public class VideoActivity extends AppCompatActivity {
+
+    private VideoView vv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video_activity);
+
+        vv = findViewById(R.id.videoView);
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.a);
+        vv.setVideoURI(videoUri);
+    }
+
+    public void start(View view) {
+        vv.start();
+    }
+
+    public void stop(View view) {
+        vv.stopPlayback();
+    }
+}
